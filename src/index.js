5cdf55916c99d2d3363dbddd9d081d49ea53be6b
matt <mat@unc.edu>
2022-07-31 16:32:42 -0400

    committed from script, no message specified

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
